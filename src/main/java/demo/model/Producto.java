6c7cef231a5bc3eec9221d1727153cdba67bee83
PabloCalandria <54867932+PabloCalandria@users.noreply.github.com>
2020-10-21 08:07:10 -0300

    Update Producto.java

--- src/main/java/demo/model/Producto.java
+++ src/main/java/demo/model/Producto.java
@@ -9,6 +9,13 @@ import javax.persistence.OneToOne;
 
 import lombok.Data;
 
+
+/**
+* Esta clase define la entidad de un producto
+* @author grupo4
+* @version Octubre 21, 2020
+*/
+
 @Entity
 @Data
 public class Producto{
@@ -22,7 +29,16 @@ public class Producto{
 //    @JoinColumn(name = "idStock", referencedColumnName = "id")
 //    private Stock stock;
 	
+	
+	/**
+	* Constructor de la clase Producto. Inicializa un producto vacio.
+	*/
+	
 	public Producto() {}
+	
+	/**
+	* Constructor de la clase Producto. Inicializa un producto con un id, un nombre y un precio.
+	*/
 	
 	public Producto(Long id, String nombre, double precio) {
 		super();
@@ -36,22 +52,47 @@ public class Producto{
 //		
 //	}
 	
+	/**
+	* Metodo que setea el nombre de un producto
+	* @param nombre El paremtro nombre es el nuevo nombre asignado.
+	*/
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	* Metodo que devuelve el nombre del producto
+	* @return String.
+	*/
 	
 	public String getNombre() {
 		return this.nombre;
 	}
 	
+	/**
+	* Metodo que setea el precio de un producto.
+	* @param precio El parametro precio es el nuevo precio del producto.
+	*/
+	
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+	
+	/**
+	* Metodo que devuelve el precio del producto.
+	* @return Double.
+	*/
 	
 	public double getPrecio() {
 		return this.precio;
 	}
 
+	/**
+	* Metodo que compara dos tipos de productos
+	* @return True o False.
+	*/
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
